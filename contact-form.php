313d41efa3526e54ef1729ebb02d90f308498744
kirkforthman <154017713+kirkforthman@users.noreply.github.com>
2023-12-17 14:46:50 -0500

    Update contact-form.php
    trying to make a simple form that works

--- contact-form.php
+++ contact-form.php
@@ -4,31 +4,11 @@ $name = $_POST["sender-name"];
 $email = $_POST["sender-email"];
 $message = $_POST["message"];
 
-require "vendor/autoload.php";
+$from= $name." // ".$email;
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMPT;
-
-$mail = new PHPMailer(true);
-
-$mail->isSMPTP();
-$mail->SMTPAuth = true;
-
-$mail->Host = "smtp.example.com";
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-$mail->Port = 587;
-
-$mail->Username = "kirkforthman@gmail.com";
-$mail->Password = "NewDl1ne!";
-
-$mail->setFrom($email, $name);
-$mail->addAddress("kirkforthman@gmail.com");
-
-$mail->Subject = "Email From Personal Site";
-$mail->Body = $message;
 
 try {
-  $mail->send();
+  mail("kirkforthman@gmail.com", "Message from Personal Site", $message, $from);
 } catch(Exception $e){
   echo "Something Went Wrong";
 }
